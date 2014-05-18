7d0c32a54ac3d9e13c56dd1cd33e97562822a17d
Christoph Brand <ch.brand@scolterius.net>
2014-05-18 17:36:56 +0200

    [Algorithms: GreedyCol] Implemented a greedy color algorithm and added a test to check for the behavior and correct code execution.

---
+++ src/scnet/grapher/algorithms/greedy_col_algorithm.py
@@ -0,0 +1,66 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from collections import defaultdict, Iterable
+
+from scnet.grapher.interfaces import (
+    IGraph,
+)
+
+
+class GreedyColAlgorithm(object):
+    """
+    Implementation of the greedy col coloring algorithm.
+    """
+
+    def __init__(
+            self,
+            graph: IGraph,
+    ):
+        self.graph = graph
+
+    def get_colors(self, amount: int) -> list:
+        """
+        Returns evenly spreaded colors.
+        :param amount: The number of colors needed.
+        """
+        return [float(i)/float(amount) for i in range(amount)]
+
+    def possible_minimum(self, data: Iterable):
+        """
+        Returns the possible minimum value. Expects a set
+        with integers. If the data does not have 0 it returns
+        that. If not it returns the minimum value not given in
+        the itertable entry.
+        """
+        if isinstance(data, set):
+            set_data = data
+        else:
+            set_data = set(data)
+
+        i = 0
+        while i in set_data:
+            i += 1
+        return i
+
+    def run(self) -> IGraph:
+        """
+        Runs the graph entry.
+        """
+
+        node_store = self.graph.node_store
+        colormap = defaultdict(lambda: None)
+        for node in node_store:
+            min_index = self.possible_minimum(
+                colormap[neighbor]
+                for neighbor in node.neighbors
+                if colormap[neighbor] is not None
+            )
+            colormap[node] = min_index
+
+        num_colors = max(colormap.values()) + 1
+        colors = self.get_colors(num_colors)
+        for node, color_index in colormap.items():
+            node.color = colors[color_index]
+        return self.graph
---
+++ test/test/scnet/grapher/algorithms/test_greedy_col_algorithm.py
@@ -0,0 +1,53 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import string
+
+from test.scnet.grapher.abstract_base_test import AbstractBaseTest
+
+from scnet.grapher.algorithms.greedy_col_algorithm import GreedyColAlgorithm
+
+
+class TestGreedyColAlgorithm(AbstractBaseTest):
+    """
+    Tests the minimum spanning tree definition.
+    """
+
+    def test_dijkstra(self):
+        """
+        Tests the algorithm with giving test data into
+        it.
+        """
+        self.nodes = self.generate_nodes(
+            *[
+                str(string.ascii_uppercase[i])
+                for i in range(9)
+            ]
+        )
+        self.edges = self.generate_edges(*[
+            ("A", "B", 2),
+            ("A", "F", 9),
+            ("A", "G", 15),
+            ("B", "C", 4),
+            ("B", "G", 6),
+            ("C", "D", 2),
+            ("C", "I", 15),
+            ("D", "E", 1),
+            ("D", "I", 1),
+            ("E", "F", 6),
+            ("E", "H", 3),
+            ("F", "H", 11),
+            ("G", "H", 15),
+            ("G", "I", 2),
+            ("H", "I", 4),
+        ])
+        self.graph = self.prepare_graph(
+            nodes=self.nodes,
+            edges=self.edges,
+        )
+        graph = GreedyColAlgorithm(self.graph).run()
+        self.assertEqual(
+            len(set(node.color for node in graph.node_store)),
+            4
+        )
